62e02ddcf96d08867592329f86e0f08ead815fa1
JIJ390 <wwdi1235@gmail.com>
2024-08-14 16:25:44 +0900

    0814 사용자 계정 생성 + SELECT 커밋

---
+++ General/Scripts/00_사용자계정생성.sql
@@ -0,0 +1,16 @@
+-- 한줄 주석
+/* 범위 주석 */
+
+-- SQL 한 줄 실행 : ctrl + Enter
+
+/* SYS 관리자 계정으로 수행하는 SQL */
+ALTER SESSION SET "_ORACLE_SCRIPT" = TRUE;
+
+-- 유저 생성
+CREATE USER KH_JIJ IDENTIFIED BY KH1234; 
+
+-- 권한 부여
+GRANT RESOURCE, CONNECT TO KH_JIJ;
+
+-- USERS 라는 공간에 20M 부여
+ALTER USER KH_JIJ DEFAULT TABLESPACE USERS QUOTA 20M ON USERS;
